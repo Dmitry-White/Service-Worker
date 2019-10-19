3212d5f92f0ce3b6f4c4993395959873cb9a43fb
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-19 16:58:05 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const init = () => {
+  const output = document.querySelector('output');
+  const MESSAGE_SW_REGISTERED = 'Service Worker registered properly';
+  const MESSAGE_SW_NOT_REGISTERED = 'Service Worker NOT registered';
+  const MESSAGE_SW_NOT_AVAILABLE = 'Service Worker NOT available';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register()
+      .then(() => {
+        output.innerHTML = MESSAGE_SW_REGISTERED;
+      })
+      .catch(() => {
+        output.innerHTML = MESSAGE_SW_NOT_REGISTERED;
+      });
+  } else {
+    output.innerHTML = MESSAGE_SW_NOT_AVAILABLE;
+  }
+};
+
+window.addEventListener('load', init);
