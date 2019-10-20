211cf18239544d65ca1838a8965ea6ab4ea48821
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-20 17:18:06 +0300

    Move events logic from sw.js

---
+++ src/events.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+self.addEventListener('install', e => {
+  console.log('Install event');
+});
+
+// eslint-disable-next-line
+self.addEventListener('activate', e => {
+  console.log('Activate event');
+});
--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,4 @@
 console.log('We are a service worker!');
 
 // eslint-disable-next-line
-self.addEventListener('install', e => {
-  console.log('Install event');
-});
-
-// eslint-disable-next-line
-self.addEventListener('activate', e => {
-  console.log('Activate event');
-});
+importScripts('events.js');
