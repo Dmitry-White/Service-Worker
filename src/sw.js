8f3004d1132bfaed6a94c19a9b9fd4f70e68b2e4
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-23 18:59:02 +0300

    Add static html generation to sw.js

--- src/sw.js
+++ src/sw.js
@@ -6,6 +6,19 @@ importScripts('events.js');
 
 self.addEventListener('fetch', (e) => {
   console.log(`Fetching ${e.request.url}`);
-  const response = new Response(`Fetching ${e.request.url}`);
+  const body = `
+  <!DOCTYPE html>
+  <title>Service Worker HTML generation</title>
+  <h1>The URL is ${e.request.url}</h1>
+  `;
+
+  const response = new Response(body, {
+    status: 200,
+    statusText: 'OK',
+    headers: {
+      'Content-type': 'text/html',
+    }
+  });
+
   e.respondWith(response);
 });
