fea434e4f70cb8f3be83646aa2bee6095720cfc0
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-23 18:50:01 +0300

    Add fetch event

--- src/sw.js
+++ src/sw.js
@@ -3,3 +3,9 @@ console.log('We are a service worker!');
 
 // eslint-disable-next-line
 importScripts('events.js');
+
+self.addEventListener('fetch', (e) => {
+  console.log(`Fetching ${e.request.url}`);
+  const response = new Response(`Fetching ${e.request.url}`);
+  e.respondWith(response);
+});
