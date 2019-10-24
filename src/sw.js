499c66e345e4722d911065b721ada8ccc609e691
Dzmitry Belaichuk <dzmitry_belaichuk@epam.com>
2019-10-24 21:21:39 +0300

    Add request data to custom response

--- src/sw.js
+++ src/sw.js
@@ -4,12 +4,20 @@ console.log('We are a service worker!');
 // eslint-disable-next-line
 importScripts('events.js');
 
+// eslint-disable-next-line
 self.addEventListener('fetch', (e) => {
   console.log(`Fetching ${e.request.url}`);
   const body = `
   <!DOCTYPE html>
   <title>Service Worker HTML generation</title>
   <h1>The URL is ${e.request.url}</h1>
+  <ul>
+    <li>Cache: ${e.request.cache}</li>
+    <li>Credential: ${e.request.credential}</li>
+    <li>Destination: ${e.request.destination}</li>
+    <li>Method: ${e.request.method}</li>
+    <li>Referrer: ${e.request.referrer}</li>
+  </ul> 
   `;
 
   const response = new Response(body, {
@@ -17,7 +25,7 @@ self.addEventListener('fetch', (e) => {
     statusText: 'OK',
     headers: {
       'Content-type': 'text/html',
-    }
+    },
   });
 
   e.respondWith(response);
